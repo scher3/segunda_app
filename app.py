2eae75c4c8dc64884843f6f3e486101d73645fdb
scher3 <schererodriiiguezz@gmail.com>
2026-01-28 21:40:11 +0000

    8 commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 pg_intro = st.Page('intro.py', title='Introducción')
 
 # Paginas EDA
-pg_eda_intro = st.Page('seccion_eda/intro_ed.py', title='Primeros Pasos')
+pg_eda_intro = st.Page('seccion_eda/intro_eda.py', title='Primeros Pasos')
 pg_eda_estadisc = st.Page('seccion_eda/estadisticos_basicos.py', title = 'Información básica')
 
 navigation_env = st.navigation(
