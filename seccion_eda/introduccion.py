5809883322a965cc1baf173e0534725d62fb1423
scher3 <schererodriiiguezz@gmail.com>
2026-01-28 14:53:20 +0000

    2commit

---
+++ seccion_eda/introduccion.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title('Mi proyecto final sobre el Bike demand')
+
+st.markdown('''
+# Modelado de patron de alquiler de bicis en ciudad Z
+Esta aplicación tiene como objetivo poder modelar el alquiler de bicis futuro en la ciudad usando
+varias tecnicas de ML
+
+** Navegación* En el menú de la izquierda tienes todo lo necesario para empezar a predecir el alquiler de bicis
+''')
