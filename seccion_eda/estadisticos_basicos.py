afaf5aa92922f3e37e893579bedf1ff84c7efd1f
scher3 <schererodriiiguezz@gmail.com>
2026-01-29 08:57:28 +0000

    11 commit

--- seccion_eda/estadisticos_basicos.py
+++ seccion_eda/estadisticos_basicos.py
@@ -2,7 +2,7 @@
 # librerias con la misma funcion de comando utilizará la ultima cargada)
 import streamlit as st
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 st.title('Lorem ipsum')
@@ -22,11 +22,32 @@ st.dataframe(df.describe().T) #da la info en un dataframe
 # Ahora vamos a trabajar sobre los estadisticos basicos, no sin antes analizar los datos
 # Estamos trabajando con datos normalizados
 # La mediana no va a ser útil
-# La media de alquileres va a ser 
+# La media de alquileres por hora es de 189.46308763450142
 st.markdown('## Estadísticos básicos')
 st.dataframe(df[['dteday','temp','windspeed','hum','cnt']].describe().T[['count','std','mean']]) # de df nosotros queremos las variables por tanto traspuesta de lo que queramos
 
-
+# Vamos a hacer una representacion de los datos
 st.markdown('## Distribución de las variables')
+
+# No funciona pues tenemos que cargar los datos en columnas
+cols = st.columns(1)
+with cols[0]:
+    fig, ax= plt.subplots()       # fig, ax= plt.figure(figsize=(8,5)) 
+    sns.histplot(df['temp'], kde=True)       # una sola figura
+    plt.title('Distribución Temp.')
+    st.pyplot(fig)                # Pyplot se encarga de cargar la figura
+
+with cols[1]:
+    fig, ax= plt.figure(figsize=(8,5))       # figura del histograma
+    sns.histplot(df['temp'], kde=True)
+    plt.title('Distribución Humedad.')
+    st.pyplot(fig)             # Pyplot se encarga de cargar la figura
+
+with cols[2]:
+    fig, ax= plt.figure(figsize=(8,5))       # figura del histograma
+    sns.histplot(df['temp'], kde=True)
+    plt.title('Distribución velocidad del viento.')
+    st.pyplot(fig)             # Pyplot se encarga de cargar la figura
+
 
 st.markdown('## Matriz de correlación')
